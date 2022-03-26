976b0f741eb58606749d263714c4b53b296d1c80
Fernando Frances <fernando.frances_ext@novartis.com>
2022-03-26 18:21:49 +0100

    Created xcode Project

---
+++ TMDB/TMDB/TMDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMDBApp.swift
+//  TMDB
+//
+//  Created by Fernando Frances  on 26/3/22.
+//
+
+import SwiftUI
+
+@main
+struct TMDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
